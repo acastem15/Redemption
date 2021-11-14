dea502bf5891932e8035809a0880b34c3f9a0332
acastem15 <a.castellanosm@uniandes.edu.co>
2021-11-14 16:37:11 -0500

    PersonajeTerminado
    Se terminaron mecancias personajes

--- Assets/Scripts/LogicaKnight.cs
+++ Assets/Scripts/LogicaKnight.cs
@@ -7,8 +7,12 @@ public class LogicaKnight : MonoBehaviour
     public float velMovimiento = 5.0f;
     public float velRotacion = 200.0f;
     private Animator anim; 
+    public Rigidbody rb;
     public float x,y; 
-    public double cronometro;
+    public bool estoyAtacando; 
+    public float impulsoGolpe = 10f; 
+    public bool space;
+
 
 
 
@@ -20,29 +24,72 @@ public class LogicaKnight : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    void FixedUpdate() 
+    {
+        if (estoyAtacando == false) {
+        
+        
+        transform.Rotate(0, x*Time.deltaTime*velRotacion, 0);
+        transform.Translate(0,0,y*Time.deltaTime*velMovimiento);
+
+        }
+    }
     void Update()
     {
         x = Input.GetAxis("Horizontal"); 
         y = Input.GetAxis("Vertical");
         
-        transform.Rotate(0, x*Time.deltaTime*velRotacion, 0);
-        transform.Translate(0,0,y*Time.deltaTime*velMovimiento);
+
 
         anim.SetFloat("VelX", x);
         anim.SetFloat("VelY", y);
 
-        if (Input.GetKeyDown("space"))
+        if (estoyAtacando == false) 
         {
-            anim.SetBool("Space", true);
+            if (Input.GetKeyDown("space"))
+            {
+                anim.SetBool("Space", true);
+                space = true; 
+            }
         }
 
+        if (Input.GetMouseButtonDown(0) && space == false)
+        {
+            anim.SetTrigger("Golpe1"); 
+            estoyAtacando = true; 
 
+        }
+        if (Input.GetMouseButtonDown(1) && space == false)
+        {
+            anim.SetTrigger("Golpe2"); 
+            estoyAtacando = true; 
+            
 
-        
+        }
+        if (Input.GetMouseButtonDown(2) && space == false)
+        {
+            anim.SetTrigger("Golpe3"); 
+            estoyAtacando = true; 
+            
+
+        }
 
     }
     void NoSpace()
     {
         anim.SetBool("Space", false);
+        space = false;
     }
+
+    void DejarGolpear()
+    {
+        estoyAtacando = false;
+    }
+    void NuevaPosicion()
+    {
+        rb.velocity = transform.forward * impulsoGolpe;
+
+    }
+
 }
